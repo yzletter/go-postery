5dbd60fcbbae4c75a2ce4945d78e277bb3133047
yzletter <yzletter@foxmail.com>
2026-03-11 15:37:35 +0800

    微服务客户端

--- client/agent.go
+++ client/agent.go
@@ -4,19 +4,19 @@ import (
 	"context"
 	"time"
 
-	code_grpc "github.com/yzletter/go-postery/api/proto/code/v1"
+	agent_grpc "github.com/yzletter/go-postery/api/proto/agent/v1"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/keepalive"
 )
 
-type codeClient struct {
+type agentClient struct {
 	conn   *grpc.ClientConn
-	client code_grpc.CodeServiceClient
+	client agent_grpc.AgentServiceClient
 }
 
-func NewCodeClient() (CodeClient, error) {
+func NewAgentClient() (AgentClient, error) {
 	// 建议：启用 ka，避免中间网络设备把长连接静默掐掉
 	ka := keepalive.ClientParameters{
 		Time:                30 * time.Second,
@@ -25,7 +25,7 @@ func NewCodeClient() (CodeClient, error) {
 	}
 
 	conn, err := grpc.NewClient(
-		CodeClientAddr,
+		AgentClientAddr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()), // 生产用 TLS
 		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),       // Jaeger
 		grpc.WithKeepaliveParams(ka),
@@ -34,28 +34,20 @@ func NewCodeClient() (CodeClient, error) {
 		return nil, err
 	}
 
-	return &codeClient{
+	return &agentClient{
 		conn:   conn,
-		client: code_grpc.NewCodeServiceClient(conn),
+		client: agent_grpc.NewAgentServiceClient(conn),
 	}, nil
 }
 
-func (client *codeClient) Close() {
+func (client *agentClient) Close() {
 	_ = client.conn.Close()
 }
 
-func (client *codeClient) Send(ctx context.Context, req *code_grpc.SendCodeRequest) (*code_grpc.SendCodeResponse, error) {
+func (client *agentClient) Chat(ctx context.Context, req *agent_grpc.ChatRequest) (*agent_grpc.ChatResponse, error) {
 	// 添加超时控制
 	ctx, cancel := context.WithTimeout(ctx, 800*time.Millisecond)
 	defer cancel()
 
-	return client.client.Send(ctx, req)
-}
-
-func (client *codeClient) Verify(ctx context.Context, req *code_grpc.CheckCodeRequest) (*code_grpc.CheckCodeResponse, error) {
-	// 添加超时控制
-	ctx, cancel := context.WithTimeout(ctx, 800*time.Millisecond)
-	defer cancel()
-
-	return client.client.Verify(ctx, req)
+	return client.client.Chat(ctx, req)
 }
---
+++ client/session.go
@@ -0,0 +1,101 @@
+package client
+
+import (
+	"context"
+	"time"
+
+	session_grpc "github.com/yzletter/go-postery/api/proto/session/v1"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
+)
+
+type sessionClient struct {
+	conn   *grpc.ClientConn
+	client session_grpc.SessionServiceClient
+}
+
+func NewSessionClient() (SessionClient, error) {
+	// 建议：启用 ka，避免中间网络设备把长连接静默掐掉
+	ka := keepalive.ClientParameters{
+		Time:                30 * time.Second,
+		Timeout:             10 * time.Second,
+		PermitWithoutStream: true,
+	}
+
+	conn, err := grpc.NewClient(
+		SessionClientAddr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()), // 生产用 TLS
+		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),       // Jaeger
+		grpc.WithKeepaliveParams(ka),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &sessionClient{
+		conn:   conn,
+		client: session_grpc.NewSessionServiceClient(conn),
+	}, nil
+}
+
+func (client *sessionClient) Close() {
+	_ = client.conn.Close()
+}
+
+func (client *sessionClient) ListByUID(ctx context.Context, req *session_grpc.UserID) (*session_grpc.Sessions, error) {
+	// 添加超时控制
+	ctx, cancel := context.WithTimeout(ctx, 800*time.Millisecond)
+	defer cancel()
+
+	return client.client.ListByUID(ctx, req)
+}
+
+func (client *sessionClient) GetSession(ctx context.Context, req *session_grpc.BothUserID) (*session_grpc.Session, error) {
+	// 添加超时控制
+	ctx, cancel := context.WithTimeout(ctx, 800*time.Millisecond)
+	defer cancel()
+
+	return client.client.GetSession(ctx, req)
+}
+
+func (client *sessionClient) GetHistoryMessagesByPage(ctx context.Context, req *session_grpc.GetHistoryMessagesByPageRequest) (*session_grpc.GetHistoryMessagesByPageResponse, error) {
+	// 添加超时控制
+	ctx, cancel := context.WithTimeout(ctx, 800*time.Millisecond)
+	defer cancel()
+
+	return client.client.GetHistoryMessagesByPage(ctx, req)
+}
+
+func (client *sessionClient) Delete(ctx context.Context, req *session_grpc.DeleteRequest) (*session_grpc.SessionEmptyResponse, error) {
+	// 添加超时控制
+	ctx, cancel := context.WithTimeout(ctx, 800*time.Millisecond)
+	defer cancel()
+
+	return client.client.Delete(ctx, req)
+}
+
+func (client *sessionClient) UpdateUnread(ctx context.Context, req *session_grpc.UpdateUnreadRequest) (*session_grpc.SessionEmptyResponse, error) {
+	// 添加超时控制
+	ctx, cancel := context.WithTimeout(ctx, 800*time.Millisecond)
+	defer cancel()
+
+	return client.client.UpdateUnread(ctx, req)
+}
+
+func (client *sessionClient) ClearUnread(ctx context.Context, req *session_grpc.ClearUnreadRequest) (*session_grpc.SessionEmptyResponse, error) {
+	// 添加超时控制
+	ctx, cancel := context.WithTimeout(ctx, 800*time.Millisecond)
+	defer cancel()
+
+	return client.client.ClearUnread(ctx, req)
+}
+
+func (client *sessionClient) CreateMessage(ctx context.Context, req *session_grpc.Message) (*session_grpc.Message, error) {
+	// 添加超时控制
+	ctx, cancel := context.WithTimeout(ctx, 800*time.Millisecond)
+	defer cancel()
+
+	return client.client.CreateMessage(ctx, req)
+}
